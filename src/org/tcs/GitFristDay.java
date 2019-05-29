e5cc82adaa0ccf2e3a82c06a3329b7e63df59c31
KiruthigaS02 <kirukith@gmail.com>
2019-05-29 09:54:14 +0530

    initial one to git

---
+++ src/org/tcs/GitFristDay.java
@@ -0,0 +1,24 @@
+package org.tcs;
+
+public class GitFristDay {
+	
+	public void emp() {
+		System.out.println("tcs");
+
+	}
+	
+	public void empid() {
+		System.out.println("123456");
+		
+	}
+	
+	public void empName() {
+		System.out.println("kiruthiga");
+
+	}
+	
+	
+
+	
+	
+}
