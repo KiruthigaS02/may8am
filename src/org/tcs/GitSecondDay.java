9f7cdb5fc413c85aa7b78578d662502525811011
KiruthigaS02 <kirukith@gmail.com>
2019-05-30 09:56:46 +0530

    changes happens

---
+++ src/org/tcs/GitSecondDay.java
@@ -0,0 +1,14 @@
+package org.tcs;
+
+public class GitSecondDay {
+
+	public void bossId() {
+System.out.println("563");
+		}
+	
+	public void empArea() {
+System.out.println("chennai");
+	}
+		
+		
+}
